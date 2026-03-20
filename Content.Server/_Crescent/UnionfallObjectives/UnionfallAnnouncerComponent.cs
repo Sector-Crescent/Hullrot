7d063f478eda3a56b86011fef117aa7d79ddc720
Emre D <emredolangac2002@hotmail.com>
2026-03-20 20:53:56 +0100

    unionfall news

--- Content.Server/_Crescent/UnionfallObjectives/UnionfallAnnouncerComponent.cs
+++ Content.Server/_Crescent/UnionfallObjectives/UnionfallAnnouncerComponent.cs
@@ -7,6 +7,6 @@ public sealed partial class UnionfallAnnouncerComponent : Component
     /// Time before capturing is allowed. This is from roundstart, so you want to add the ship grace period ON TOP of the value you want.
     /// </summary>
     [DataField]
-    public float GracePeriod = 1200f; //20 minutes for unionfall
+    public float GracePeriod = 900f; //20 minutes for unionfall
 
 }
