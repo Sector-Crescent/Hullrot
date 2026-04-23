248446d292abbb504240b35d90a59c2cba7608f7
evergreenBloom <ofthefathoms@gmail.com>
2026-04-22 19:50:45 -0500

    .

--- Content.Server/_Crescent/SelfDeleteTimer/SelfDeleteInSpaceComponent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared.EventScheduler;
-
-namespace Content.Server._Crescent.HullrotSelfDeleteTimer;
-
-[RegisterComponent]
-public sealed partial class SelfDeleteInSpaceComponent : Component
-{
-    
-}
--- Content.Server/_Crescent/Station/StationLifeHeuristicComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Server._Crescent.Station;
-
-[RegisterComponent]
-public sealed partial class StationLifeHeuristicComponent : Component
-{
-
-}
