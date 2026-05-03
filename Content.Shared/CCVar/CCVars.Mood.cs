4c586c4f8132d32cef373710637e45fcf337d345
trcmrt <trcmrtbusiness@gmail.com>
2026-05-03 17:44:57 -0400

    how curious

--- Content.Shared/CCVar/CCVars.Mood.cs
+++ Content.Shared/CCVar/CCVars.Mood.cs
@@ -12,7 +12,7 @@ public sealed partial class CCVars
 #if RELEASE
         CVarDef.Create("mood.enabled", true, CVar.SERVER);
 #else
-        CVarDef.Create("mood.enabled", false, CVar.SERVER);
+        CVarDef.Create("mood.enabled", true, CVar.SERVER);
 #endif
 
     public static readonly CVarDef<bool> MoodIncreasesSpeed =
@@ -25,5 +25,5 @@ public sealed partial class CCVars
         CVarDef.Create("mood.modify_thresholds", false, CVar.SERVER);
 
     public static readonly CVarDef<bool> MoodVisualEffects =
-        CVarDef.Create("mood.visual_effects", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+        CVarDef.Create("mood.visual_effects", false, CVar.CLIENTONLY | CVar.ARCHIVE);
 }
